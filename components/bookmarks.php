d24e99415b6057ce1f31e73b86d36f6bd69197f6
ImAmish <amerzahirovic@outlook.com>
2019-09-05 13:35:00 +0200

    links fixed

--- components/bookmarks.php
+++ components/bookmarks.php
@@ -15,7 +15,7 @@
 		$bookmarks = DB::query('SELECT * FROM bookmarks');		
 		
 		foreach ($bookmarks as $bookmark){
-			echo "<div style='padding: 5px;'>";
+			echo "<div  style='padding: 5px;'>";
 			echo "<img  src='" . $bookmark['logo_path'] . "'></img>";
 			echo "<a target='_blank' href='".$bookmark['url']."'> <p>" . $bookmark['name'] . "</p></a>";
 			
@@ -26,9 +26,11 @@
 
 	<form action="#" method="POST">
 		<input type="text" name="name" placeholder="Name Website" >
-		<input type="text" name="url" placeholder="Name Url">
+		<input  type="text" name="url" placeholder="Name Url">
 		<input type="text" name="logo_path" placeholder="Link Picture">
-		<button type="submit" name="submit">Add Bookmark</button>
+		<br>
+		<br>
+		<button  style="padding-top: 5px;" class="btn btn-success" type="submit" name="submit">Add Bookmark</button>
 	</form>
 
 	
